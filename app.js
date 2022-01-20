8ad39a896c9b41b29251edf9fd0d9554dab1fa6c
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-01-19 22:40:48 -0500

    finished 9.2

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+// this is required to run the fs module. it allows the app.js file to access the fs module's functions through the fs assignment
+// when ever we use a module we need to have a require. We can create and use local modules as well. 
+const fs = require('fs');
+
+// with this statemen, the object in the module.exports assignment will be reassigned to the generatePage variable in the app.js file. We use a relative path to get to file.
+const generatePage = require('./src/page-template.js');
+
 // process is a global object that represents everything going on. it is always present. if you console.log(process) you'll see that its a gigantic object holding data providing the context of where the app was executed.
 // the argv property of process is an array that holds exactly what was typed into the command line upon execution so that we can capture that data and use it in the app.
 // .slice() an array method that returns a brand-new array, here the brand new array is based on process.argv starting at the third index (index 2 in the 0 based array)-this skips over the node and the project file items in the array, the first two.
@@ -5,34 +12,20 @@
 // we don't actually manipulate process.argv, but rather create a new array based on the values from the third index and on.
 // const keyword allows us to create variables that can't be reassigned a value
 // const for strings and numbers cannot be altered, because the reference is to the content. For arrays and objects however the content can be altered because the reference is to the container, not the content
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
+// this variable assignment is called assignment deconstruction. It assigns elements of an array to variable names in a single expression. it does so sequentially/ So the first thing will be given variable name nombre, and the secpnd thing will be given the variable name github.
+const [nombre, github] = profileDataArgs;
 
-// arrow functions are a new and alternative way to create functions. These are more concise. They do not required the keyword function. If you only have one parameter you don't even need paranthesis aroud. Technically we don't need the paranthesis around profileDataArr. We can even remove {} if we only need to perform one action. This is an implicit return, we dont have to use a return statement if were performing once action.
-// let is for variables that can change over time. makes code easier to understand.
-// let and const are block-scoped.variables that are created within any block of code {} will not exist outside the block of code. They won't override a variable created outside of the braces either
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+// in the fs module, all functions have synchronous and asynchronous versions, including fs.writeFile and fs.writeFileSync()
+// in a fs.writefile() there are three arguents. The first argument is the file name. The second argument is the data that's being written: the HTML string template. The third argument is the callback function that will handle any errors as well as the success message.
+// notice the arrow function, no parantheses enclose the argument. When there is only one argument the () are optional.
+fs.writeFile('./index.html', generatePage(nombre, github), err => {
+    // in the call back function, a conditional statement checks for the err being returned. if err exist, an error message is displayed. This creates an exception and stops the execution of the code.
+    // if there is an err the resulting error in the terminal --> This message concatenates two text properties of the Error object that is returned, name and message. In this case, name is "TypeError", and message is "Callback must be a function. Received undefined." It's important to know this, for you may wish to retrieve properties of the Error object while you're developing your applications, and these two are central to identifying problems.
+    // example of error message in TERM: TypeError [ERROR_INVALID_CALLBACK]: Callback must be a function. Received undefined
+    // example of another type of error message ReferenceError: fs is not defined
+    if (err) throw new Error(err);
 
-    console.log('===================');
-
-    // forEach() is an array method that accepts a function as an argument and executes that function on each element of the array, using the value of the element at that iteration as its argument
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-    });
-
-    console.log('====================');
-
-    // using an arrow function, we can avoid using the function keyword, parenthesis around the function parameter, and the curly braces used to wrap a function, as we're only performing one action in the function. We can keep simpler tasks on one line of code.
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-
-};
-
-
-
-
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
---
+++ src/page-template.js
@@ -0,0 +1,55 @@
+
+// arrow functions are a new and alternative way to create functions. These are more concise. They do not required the keyword function. If you only have one parameter you don't even need paranthesis aroud. Technically we don't need the paranthesis around profileDataArr. We can even remove {} if we only need to perform one action. This is an implicit return, we dont have to use a return statement if were performing once action.
+// let is for variables that can change over time. makes code easier to understand.
+// let and const are block-scoped.variables that are created within any block of code {} will not exist outside the block of code. They won't override a variable created outside of the braces either
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('===================');
+
+//     // forEach() is an array method that accepts a function as an argument and executes that function on each element of the array, using the value of the element at that iteration as its argument
+//     profileDataArr.forEach((profileItem) => {
+//         console.log(profileItem)
+//     });
+
+//     console.log('====================');
+
+//     // using an arrow function, we can avoid using the function keyword, parenthesis around the function parameter, and the curly braces used to wrap a function, as we're only performing one action in the function. We can keep simpler tasks on one line of code.
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+
+// };
+
+// this function returns a string
+// since there is no parameter here we have to put parantheses. When there is only one parameter we do not have to put parantheses.
+// notice no return. When a function has only a single statement, the curly braces are unnecessary and the return statement is implied.
+
+// printProfileData(profileDataArgs);
+
+// template literals are enclosed in ` back ticks. these allow use to insert variables by using the ${variable} syntax
+// interpolation is the substitution of text for a variable we build into the string
+// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+// by using enter in between the `s, like you would in a word processor you can create mutli line strings.
+const generatePage = (nombre, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
+
+    <body>
+        <h1>${nombre}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+};
+
+// in order to use functions from one module inside another, we use the related statements module.exports and require.
+// in the source file that has the functions we want to make available to other files, we use module.export. at the top of the destination file we put require
+module.exports = generatePage;
